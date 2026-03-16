083576654be60adff645ef6ed4419a9082bd7cd5
Suranbaev Baitur <baitur1912@kstu.kg>
2026-03-16 23:06:41 +0600

    feature commit 2

--- main.go
+++ main.go
@@ -2,3 +2,4 @@ package main
 func main() { println("Hello World") }
 func add(a, b int) int { return a + b }
 func multiply(a, b int) int { return a * b }
+func subtract(a, b int) int { return a - b }
